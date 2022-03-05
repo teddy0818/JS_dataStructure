42752726323f49b5c10e7198dd7abacf07fdf4af
Byeongjin Hwang <taddy0818@naver.com>
2022-03-05 18:32:07 +0900

    세 수 중 최솟값

---
+++ section_1/세 수 중 최솟값.js
@@ -0,0 +1,25 @@
+/*
+
+100이하의 자연수 A, B, C를 입력받아 세 수 중 가장 작은 값을 출력하는 프로그램을 작성하 세요.(정렬을 사용하면 안됩니다)
+
+*/
+
+function solution(a, b, c) {
+    let answer = 0;
+
+    if (a>b) {
+        answer = b;
+    } else {
+        answer = a;
+    }
+
+    if(b>c) {
+        answer = c;
+    } else {
+        answeer = b;
+    }
+
+    console.log(answer);
+}
+
+solution(1,99,3);
