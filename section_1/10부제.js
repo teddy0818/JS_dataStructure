c906e42d975d5f3e6d9d637cd95f6aac71c11b17
Byeongjin Hwang <taddy0818@naver.com>
2022-07-02 19:07:18 +0900

    10부제

---
+++ section_1/10부제.js
@@ -0,0 +1,47 @@
+
+function solution(day, arr){         
+    let answer = 0;
+    
+    for (let val of arr) {
+        let str = String(val);
+        let final_num = str.charAt(str.length-1);
+
+        if(day == final_num) {
+            answer++;
+        }
+    }
+   
+    
+    return answer;
+}
+
+let arr = [25, 23, 11, 47, 53, 17, 33];
+
+console.log(solution(5, arr));
+
+/*
+    <Tip>
+
+    어떤 숫자든지 10으로 나누면 일의 자리가 나온다
+    ex) 10 % 2567 : 결과값은 7
+
+*/
+    
+
+/*
+    <답안>
+    
+              function solution(day, arr){
+                let answer=0;
+                for(let x of arr){
+                    if(x%10==day) answer++;
+                }
+                
+                return answer;
+            }
+            
+            arr=[25, 23, 11, 47, 53, 17, 33];
+            console.log(solution(3, arr));
+    
+
+*/
