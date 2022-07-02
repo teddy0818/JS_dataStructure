7c0dc08248753ccc1f780cf7486bc1c9ea9b929e
Byeongjin Hwang <taddy0818@naver.com>
2022-07-02 16:18:36 +0900

    최솟값 구하기 - 답안 추가

--- section_1/최솟값 구하기.js
+++ section_1/최솟값 구하기.js
@@ -12,4 +12,23 @@ function solution(arr){
 }
 
 let arr=[5, 7, 1, 3, 2, 9, 11];
+
 console.log(solution(arr));
+
+/* 
+/* 
+
+     function solution(arr){         
+                let answer, min=Number.MAX_SAFE_INTEGER;
+                for(let i=1; i<arr.length; i++){
+                    if(arr[i]<min) min=arr[i];
+                }
+                answer=min;
+                return answer;
+
+            }
+
+            let arr=[5, 7, 1, 3, 2, 9, 11];
+            console.log(solution(arr));
+
+*/
