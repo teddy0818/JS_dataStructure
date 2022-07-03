9ce3acca777912d9bcc0bc1bb5493154c7a6cc37
Byeongjin Hwang <taddy0818@naver.com>
2022-07-03 22:58:03 +0900

    10부제 예제 추가

--- section_1/10부제.js
+++ section_1/10부제.js
@@ -16,8 +16,11 @@ function solution(day, arr){
 }
 
 let arr = [25, 23, 11, 47, 53, 17, 33];
+let arr2 = [12, 20, 54, 30, 87, 91, 30];
+
 
 console.log(solution(5, arr));
+console.log(solution(0, arr2));
 
 /*
     <Tip>
