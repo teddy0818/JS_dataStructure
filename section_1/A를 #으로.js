c59cb766416926b58c4f17ba4512ead4f06c3a69
Byeongjin Hwang <taddy0818@naver.com>
2022-07-06 23:19:04 +0900

    A를 #으로 - 답안추가

--- section_1/A를 #으로.js
+++ section_1/A를 #으로.js
@@ -28,6 +28,17 @@ console.log(solution(str));
 /*
     <답안>
     
+         function solution(s){
+                let answer="";
+                for(let x of s){
+                    if(x=='A') answer+='#';
+                    else answer+=x;
+                }
+                return answer;
+            }
+            
+            let str="BANANA";
+            console.log(solution(str));
 
     
 
