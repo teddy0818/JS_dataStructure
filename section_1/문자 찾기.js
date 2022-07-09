e150bb93108fab7dc6644f8e2d67860c6ca6e47d
Byeongjin Hwang <taddy0818@naver.com>
2022-07-09 17:21:24 +0900

    문자 찾기

---
+++ section_1/문자 찾기.js
@@ -0,0 +1,34 @@
+
+function solution(sentence, word){         
+    let answer = 0;
+
+    for(let i=0; i<sentence.length; i++) {
+        if(sentence[i] == word) answer++;
+    }
+
+    return answer;
+}
+
+console.log(solution('COMPUTERPROGRAMMING', 'R'));
+
+
+/*
+    <Tip>
+
+    split 함수(특정 문자 기준으로 문자열을 배열화) 활용해서 문제를 풀수도 있음
+
+*/
+    
+
+/*
+    <답안>
+
+       function solution(s, t){
+            let answer = s.spilt(t);
+            return answer;
+        }
+            
+        let str="COMPUTERPROGRAMMING";
+        console.log(solution(str, 'R'));
+
+*/
