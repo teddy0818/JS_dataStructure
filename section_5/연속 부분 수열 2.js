bdc106ae1e330ae665a615fea32b2ef1c7e112b7
teddy0818 <taddy0818@naver.com>
2024-01-11 18:50:59 +0900

    section_5 - 연속 부분 수열 2 - 수정

--- section_5/연속 부분 수열 2.js
+++ section_5/연속 부분 수열 2.js
@@ -1,31 +1,22 @@
 
-function solution(n, m, arr) {
+function solution(m, arr) {
 
     let answer = 0;
     let sum = 0;
     let lt = 0;
 
-    for(let rt=0; rt<n; rt++) {
+    for(let rt=0; rt<arr.length; rt++) {
         sum += arr[rt];
-        if(sum<m) {
-            answer++;
-        }
-        if(rt!=0 && arr[rt]<=m) {
-            answer++;
-        }
-        while(sum>=m) {
+        while(sum>m) {
             sum -= arr[lt++];
-            if(sum<=m) {
-                answer++;
-            }
         }
+        answer += (rt-lt+1);
     }
-
     
     return answer;
 }
 
-console.log(solution(5, 5, [1, 3, 1, 2, 3]));
+console.log(solution(5, [1, 3, 1, 2, 3]));
 
 /*
     <Tip>
@@ -35,7 +26,20 @@ console.log(solution(5, 5, [1, 3, 1, 2, 3]));
 
 /*
     <정답> 
-   
+    function solution(m, arr){
+        let answer=0, sum=0, lt=0;
+        for(let rt=0; rt<arr.length; rt++){
+            sum+=arr[rt];
+            while(sum>m){
+                sum-=arr[lt++];
+            }
+            answer+=(rt-lt+1);
+        }               
+        return answer;
+    }
+    
+    let a=[1, 3, 1, 2, 3];
+    console.log(solution(5, a));
     
         
 */
