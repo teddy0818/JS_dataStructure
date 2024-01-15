81105ef923038c30f11d869d655fa6e93df0af3c
teddy0818 <taddy0818@naver.com>
2024-01-15 16:23:16 +0900

    section_5 - 학급 회장 - 수정

--- section_5/학급 회장.js
+++ section_5/학급 회장.js
@@ -34,18 +34,24 @@ console.log(solution(15, "BACBACCACCBDEDE"));
 /*
 
     <정답> 
-    function solution(k, arr){
-        let answer, sum=0;
-        for(let i=0; i<k; i++) sum+=arr[i];
-        answer=sum;
-        for(let i=k; i<arr.length; i++){
-            sum+=(arr[i]-arr[i-k]);
-            answer=Math.max(answer, sum);
-        }                    
+    function solution(s){  
+        let answer;
+        let sH = new Map();
+        for(let x of s){
+            if(sH.has(x)) sH.set(x, sH.get(x)+1);
+            else sH.set(x, 1);
+        }
+        let max=Number.MIN_SAFE_INTEGER;
+        for(let [key, val] of sH){
+            if(val>max){
+                max=val;
+                answer=key;
+            }
+        }
         return answer;
     }
-    
-    let a=[12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
-    console.log(solution(3, a));
+
+    let str="BACBACCACCBDEDE";
+    console.log(solution(str));
         
 */
