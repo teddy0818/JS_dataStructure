46452b25322cbe411edfb55dbb76d2f2e52911ec
teddy0818 <taddy0818@naver.com>
2024-01-21 10:03:49 +0900

    section_6 - 크레인 인형뽑기(카카오기출)

---
+++ section_6/크레인 인형뽑기(카카오기출).js
@@ -0,0 +1,52 @@
+function solution(board, moves) {
+  let answer = 0;
+  let basket = [];
+
+  for (let move of moves) {
+    let colIndex = move - 1;
+    let test = "";
+    for (let j = 0; j < board.length; j++) {
+      test += " " + board[j][colIndex];
+      console.log(test);
+      if (board[j][colIndex] !== 0) {
+        if (
+          basket.length > 0 &&
+          basket[basket.length - 1] == board[j][colIndex]
+        ) {
+          basket.pop();
+          answer += 2;
+        } else {
+          basket.push(board[j][colIndex]);
+        }
+        console.log(basket);
+        board[j][colIndex] = 0;
+        break;
+      }
+    }
+    console.log();
+  }
+
+  return answer;
+}
+
+const paramBoard = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 3],
+  [0, 2, 5, 0, 1],
+  [4, 2, 4, 4, 2],
+  [3, 5, 1, 3, 1],
+];
+const paramMoves = [1, 5, 3, 5, 1, 2, 1, 4];
+console.log(solution(paramBoard, paramMoves));
+
+/*
+      <Tip>
+      - Stack 이용해서 다시 풀기
+
+*/
+
+/*
+  
+      <정답> 
+     
+ */
