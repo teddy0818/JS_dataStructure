c3517d1dab507dd9c4a957afdc4dae922391877c
teddy0818 <taddy0818@naver.com>
2024-01-30 16:44:09 +0900

    section_7 - 버블정렬 (정답기입)

--- section_7/버블정렬.js
+++ section_7/버블정렬.js
@@ -14,18 +14,28 @@ function solution(arr) {
   return arr;
 }
 
-console.log(solution([7, 4, 5, 1, 3]));
+console.log(solution([7, 4, 5, 13, 3]));
 
 /*
         <Tip>
         - 버블정렬
 
-  
   */
 
 /*
-    
         <정답> 
+        function solution(arr){
+            let answer=arr;
+            for(let i=0; i<arr.length-1; i++){
+                for(let j=0; j<arr.length-i-1; j++){
+                    if(arr[j]>arr[j+1]){
+                        [arr[j], arr[j+1]]=[arr[j+1], arr[j]];
+                    }
+                }   
+            } 
+            return answer;
+        }
 
-
+        let arr=[13, 5, 11, 7, 23, 15];
+        console.log(solution(arr));
     */
