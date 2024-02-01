5fe69d99197cfad1d1a7fd6f2fd370b85168b2ad
teddy0818 <taddy0818@naver.com>
2024-02-01 19:08:44 +0900

    section_7 - Special Sort 정답기입

--- section_7/Special Sort.js
+++ section_7/Special Sort.js
@@ -25,5 +25,18 @@ console.log(solution([1, 2, 3, -3, -2, 5, 6, -6]));
 
 /*
         <정답> 
-        
+        function solution(arr){
+            let answer=arr;
+            for(let i=0; i<arr.length-1; i++){
+                for(let j=0; j<arr.length-i-1; j++){
+                    if(arr[j]>0 && arr[j+1]<0){
+                        [arr[j], arr[j+1]]=[arr[j+1], arr[j]];
+                    }
+                }   
+            } 
+            return answer;
+        }
+
+        let arr=[1, 2, 3, -3, -2, 5, 6, -6];
+        console.log(solution(arr));
     */
